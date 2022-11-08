5b930c8c01677d92274f425942058abfe74f11fd
Eliecer zuniga <eliecermiguelzuor@gmail.com>
2022-11-08 15:00:52 -0500

    static uploads commit

--- index.ts
+++ index.ts
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
   res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
+app.use(express.static("uploads"));
 
 app.use("/api", publicRoutes);
 app.use("/api", auth, privateRoutes);
